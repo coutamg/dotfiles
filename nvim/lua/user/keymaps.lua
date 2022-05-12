a36587d76dc6600738009600bde2c72676d7f245
coutamg <329826418@qq.com>
2022-05-12 17:52:56 +0800

    modify keymap

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -40,8 +40,8 @@ keymap("n", "<C-s>", ":w<cr>", opts)
 -- exit cur window
 keymap("n", "<leader>q", ":q<cr>", opts)
 -- delete cur buffer
-keymap("n", "<leader>d", ":bdelete<cr>", opts)
-keymap("n", "<leader>D", ":bdelete<cr>", opts)
+keymap("n", "<S-d>", ":bdelete<cr>", opts)
+-- keymap("n", "<leader>D", ":bdelete<cr>", opts)
 -- exit whole program
 keymap("n", "ZZ", ":lua require('user.utils').SaveAndExit()<cr>", opts)
 -- remap macro record key
